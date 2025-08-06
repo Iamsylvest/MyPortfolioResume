50332bb5bd97a292b0a5aaed032047de8d6c352d
Iamsylvest <madelosylvest@gmail.com>
2025-08-06 20:24:44 +0800

    Changes of profile and its position

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,8 +3,8 @@
     <div class="relative space-y-5 sm:left-32">
       <h1 class="text-4xl font-bold text-center sm:text-left">Hi! I am Sylvest Madelo</h1>
       <p class="text-center sm:text-left">
-        "Aspiring Web Developer passionate about both frontend and backend — building and learning
-        one project at a time."
+        "Aspiring Web Developer passionate about both frontend and backend <br />
+        <span> — building and learning one project at a time."</span>
       </p>
 
       <div class="flex justify-center sm:justify-start">
@@ -48,11 +48,13 @@
         </a>
       </div>
     </div>
-    <div class="relative left-0 flex justify-end sm:justify-start sm:left-64">
+    <div
+      class="relative flex items-center justify-center sm:justify-start sm:translate-x-32 top-12 sm:top-0"
+    >
       <img
-        src="@/assets/img/profile.png"
+        src="@/assets/img/profile2.jpg"
         alt="Profile"
-        class="rounded-full w-72 h-72 cover lg:w-72 lg:h-96"
+        class="border rounded-lg w-96 h-96 cover lg:w-96 lg:h-86"
       />
     </div>
   </body>
